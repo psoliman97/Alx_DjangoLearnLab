8f9830109c3d48e51195b21ae19ccce6b5ef53f5
Sully <salmaamoohamedsm@gmail.com>
2025-08-02 04:40:34 +0300

    Add files via upload

---
+++ Introduction_to_Django/LibraryProject/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,17 @@
+from models import Author, Book, Library, Librarian
+
+def get_books_by_author(author_name, author):
+    Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
+
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
+
+def get_librarian_for_library(library_name):
+    Librarian.objects.get(library_name="")
+    librarian = Librarian.objects.librarian
+    return librarian
+
