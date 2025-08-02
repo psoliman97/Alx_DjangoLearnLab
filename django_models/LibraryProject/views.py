3a1507c57b76432944497b8d671337b5b56a627f
Sully <salmaamoohamedsm@gmail.com>
2025-08-02 04:48:00 +0300

    Add files via upload

---
+++ django_models/LibraryProject/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+
+# relationship_app/views.py
+# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    context = {'books_list': books}
+    return render(request, "relationship_app/list_books.html", context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = 'library' 
+
+ 
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['books'] = self.object.books.all()
+    return context
+
+
+def register (createview):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+
