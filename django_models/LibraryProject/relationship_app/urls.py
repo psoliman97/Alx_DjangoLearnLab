93c3bd2f6c597836dbadfcf465ab2aa9738eb8a6
Sully <salmaamoohamedsm@gmail.com>
2025-08-02 04:45:16 +0300

    Add files via upload

---
+++ django_models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import (
+    list_books,
+)
+from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView as loginview, LogoutView as logoutview
+from django.contrib.auth.decorators import login_required
+from .views import register_librarian, get_librarian_for_library
+from .import views
+
+urlpatterns = [
+    path('register/', views.register.as_view(), name='register'),
+    path('login/', loginview.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', logoutview.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('library/', LibraryDetailView.as_view(), name='library_detail'),
+    path('list_books/', list_books, name='list_books'),
+    path('admin/', views.admin.view, name='admin_view'),
+    path('librarian/', views.librarian.view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('add/', views.add_book, name='add_book'),
+    path('edit/<int:pk>/'),
+    path('delete/<int:pk>/', views.delete_book, name='delete_book'),
+]
