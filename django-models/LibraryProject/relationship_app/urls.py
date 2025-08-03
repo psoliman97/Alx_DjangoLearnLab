0404fbba40052d9bfa373c87f90bcd18a55ac895
Sully <salmaamoohamedsm@gmail.com>
2025-08-04 02:06:30 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,12 +10,13 @@ from .import views
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='')),
     path('logout/', LogoutView.as_view(template_name='')),
+    path('Admin-dashboard/', views.Admin_only_view, name='Admin_dashboard'),
     path('register/', views.register.as_view(), name='register'),
     #path('login/', loginview.as_view(template_name='relationship_app/login.html'), name='login'),
     #path('logout/', logoutview.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('library/', LibraryDetailView.as_view(), name='library_detail'),
     path('list_books/', list_books, name='list_books'),
-    path('admin/', views.admin.view, name='admin_view'),
+    #path('admin/', views.admin.view, name='admin_view'),
     path('librarian/', views.librarian.view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
     path('add/', views.add_book, name='add_book'),
@@ -23,4 +24,5 @@ urlpatterns = [
     path('delete/<int:pk>/', views.delete_book, name='delete_book'),
 
 ]
+
 
