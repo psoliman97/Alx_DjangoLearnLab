d5bcc111079c0a292934ecbdbaa5f5f2e0c0c854
Sully <salmaamoohamedsm@gmail.com>
2025-08-03 23:36:21 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
 from django.urls import reverse_lazy
@@ -56,6 +56,7 @@ def is_member(user):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
 
 
 
