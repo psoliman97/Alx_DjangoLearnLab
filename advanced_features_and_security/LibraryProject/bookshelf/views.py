7e16d0f1470b7cbb86148e2206ef98c4c5d06b83
Sully <salmaamoohamedsm@gmail.com>
2025-08-04 14:44:07 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import has_permission
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Book
+from .forms import ExampleForm
 
 ["book-list", "raise_exception", "books"]
 ["from .forms import ExamplesForm"]
@@ -19,4 +20,5 @@ def book_list(request):
     books = Book.objects.all()
     context = {'book_list': books}
     return render(request, 'relationship_app/book_list.html', context)
+
 
