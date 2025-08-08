bea210f51e039315a9d4c157f1591d45cab3c96f
Peter Soliman <petersoliman94@gmail.com>
2025-08-08 18:22:20 +0300

    u

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import  UserCreationForm , AuthenticationForm # Required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login  # Explicitly added as required
 
 # Register View
