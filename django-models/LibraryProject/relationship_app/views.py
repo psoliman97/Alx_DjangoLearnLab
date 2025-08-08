5ba33ac973b00c0ef1d0eb227d421c88cc6f385b
Peter Soliman <petersoliman94@gmail.com>
2025-08-08 15:46:21 +0300

    u

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Book
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 
 def list_books(request):
     books = Book.objects.all()
